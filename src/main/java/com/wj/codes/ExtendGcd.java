91d6c880df6aa81561ab81ca0789fe6f63c12e4d
wangJian <jianw@hupofintech.com>
2019-12-18 17:50:11 +0800

    扩展欧几里得

---
+++ src/main/java/com/wj/codes/ExtendGcd.java
@@ -0,0 +1,52 @@
+package com.wj.codes;
+
+public class ExtendGcd {
+
+    public static void main(String[] args) {
+        ExtendGcd extendGcd = new ExtendGcd();
+        Result result = extendGcd.getXY(3, 4);
+        System.out.println(result);
+    }
+
+    public static class Result {
+
+        public int x;
+        public int y;
+
+        public Result() {
+        }
+
+        public Result(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return this.x + " , " + this.y;
+        }
+    }
+
+    public Result getXY(int a, int b) {
+        if (b == 0) {
+            return new Result(1, 0);
+        }
+
+        Result preResult = getXY(b, a % b);
+
+        Result result = new Result();
+        result.x = preResult.y;
+        result.y = preResult.x - (a / b) * preResult.y;
+
+        return result;
+    }
+
+    public int gcd(int a, int b) {
+
+        if (b == 0) {
+            return a;
+        }
+
+        return gcd(b, a % b);
+    }
+}
